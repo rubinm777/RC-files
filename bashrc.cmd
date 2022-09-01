27343de2ef14ff76ea77c0896f9fe72f899a70b4
Rubin <ruma9205@colorado.edu>
2022-09-01 10:20:47 +0530

    Add comments
    Added comments to better understand bashrc.cmd

--- bashrc.cmd
+++ bashrc.cmd
@@ -6,8 +6,11 @@ set TEST_VAR=something
 :: Commands:
 ::
 :: Aliases:
+::
+:: Navigation Aliases:
 doskey gt=cd "C:\Users\MATHEW\Desktop\Rubin Work\Random\Python Scripts"
 doskey pt=cd "C:\Users\MATHEW\Desktop\Rubin Work\Courses\Machine Learning\MLCourse"
+:: BASH Command Aliases:
 doskey ls=dir $*
 doskey mv=move $*
 doskey cp=copy $*
